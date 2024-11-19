d7a3b098738b2d02c7fc9c53f63f957298697d50
Kyle <kmusgrove2289@gmail.com>
2024-11-19 09:24:11 -0500

    Convert Open-GroundingDino to a regular folder

---
+++ Open-GroundingDino/train_dist.sh
@@ -0,0 +1,13 @@
+CFG=$1
+DATASETS=$2
+OUTPUT_DIR=$3
+
+# Set the environment variable for CUDA
+export CUDA_VISIBLE_DEVICES=0
+
+python main.py \
+    --config_file ${CFG} \
+    --datasets ${DATASETS} \
+    --output_dir ${OUTPUT_DIR} \
+    --pretrain_model_path /home/km/content/groundingdino_swint_ogc.pth \
+    --options text_encoder_type="/home/km/content/bert"
